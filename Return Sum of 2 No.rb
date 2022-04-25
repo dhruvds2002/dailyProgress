c57cdb820119e54710c09e38a61f0129aad59cb7
dhruvds2002 <103987671+dhruvds2002@users.noreply.github.com>
2022-04-25 14:55:57 +0530

    Create Return Sum of 2 No.rb

---
+++ Return Sum of 2 No.rb
@@ -0,0 +1,12 @@
+# Return Sum of 2 Numbers
+=begin
+puts"Enter First Number"
+a=gets.chomp.to_i
+puts"Enter Second Number"
+b=gets.chomp.to_i
+
+def sum(n1,n2)
+  return n1+n2
+end
+puts sum(a,b)
+=end
